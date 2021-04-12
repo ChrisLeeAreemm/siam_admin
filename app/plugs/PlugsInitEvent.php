eb6fec0f81cd56664a801d61ba75b9a5d8b90a5f
宣言 <59419979@qq.com>
2021-04-12 17:39:06 +0800

    插件说明

--- app/plugs/PlugsInitEvent.php
+++ app/plugs/PlugsInitEvent.php
@@ -9,9 +9,9 @@ class PlugsInitEvent
 {
     public static function app_init()
     {
+        // TODO 注入路由 [1.获取插件列表(带安装状态) 2.启用插件 3.停用插件 ]
 
-        // TODO 遍历plugs目录，查看是否已经安装
-        // 执行已经安装过的plugs init
+        // TODO 遍历plugs目录，执行已经安装过的plugs init
 
         (new Plugs())->init();
     }
