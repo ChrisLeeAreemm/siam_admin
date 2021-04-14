06d62a3f36a89cbb891fc7305e4e93357c83cc4d
Chris <386273094@qq.com>
2021-04-14 10:11:14 +0800

    去除路由

--- route/app.php
+++ route/app.php
@@ -15,6 +15,3 @@ Route::get('think', function () {
 });
 
 Route::get('hello/:name', 'index/hello');
-Route::group(function (){
-    Route::get('user/get_list', 'AdminUsersController/get_list');
-})->prefix('admin.');
