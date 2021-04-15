cf594be6386b9ca895eca4323b955566b276b519
宣言 <59419979@qq.com>
2021-04-15 17:11:28 +0800

    add: plugs base controller

--- app/plugs/PlugsBaseController.php
+++ app/plugs/PlugsBaseController.php
@@ -9,4 +9,19 @@ use app\BaseController;
 abstract class PlugsBaseController extends BaseController
 {
 
+    function get_list()
+    {
+    }
+
+    function add()
+    {
+    }
+
+    function edit()
+    {
+    }
+
+    function delete()
+    {
+    }
 }
