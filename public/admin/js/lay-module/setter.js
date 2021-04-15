02b8f403c84151c626a9640334ac5cb9d787c397
宣言 <59419979@qq.com>
2021-04-15 09:05:39 +0800

    add: errorCode页面;git 处理文件

--- public/admin/js/lay-module/setter.js
+++ public/admin/js/lay-module/setter.js
@@ -4,7 +4,7 @@
 
 layui.define(['layer'], function (exports) {
     exports('setter', {
-        Api : "http://www.siamadmin.local/"
+        Api : "http://127.0.0.1:8000/"
         ,name: 'Siam_Admin'
         , tableName: 'Siam_Admin' //本地存储表名
         //自定义请求字段
