b65740616496d55e394ca4b2c854edcd20913a61
宣言 <59419979@qq.com>
2021-04-16 17:47:50 +0800

    add: plugs base service

--- app/plugs/base/service/PlugsBaseHelper.php
+++ app/plugs/base/service/PlugsBaseHelper.php
@@ -5,14 +5,42 @@ namespace app\plugs\base\service;
 
 
 use app\model\PlugsStatusModel;
+use Siam\Component\Di;
 
 class PlugsBaseHelper
 {
     /**
      * 某插件是否安装，用于插件安装逻辑  检测依赖插件的安装
      * @param $plugs_name
+     * @return bool
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
      */
-    public static function isInstall($plugs_name)
+    public static function isInstall($plugs_name): bool
     {
+        $has = PlugsStatusModel::where([
+            'plugs_name' => $plugs_name
+        ])->find();
+
+        Di::getInstance()->set("plugs_".$plugs_name, $has);
+
+        return !!$has;
     }
+
+    /**
+     * 获取插件状态
+     * @throws \Throwable
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\db\exception\DbException
+     */
+    public static function getPlugsStatus($plugs_name)
+    {
+        return Di::getInstance()->get("plugs_".$plugs_name) ?? PlugsStatusModel::where([
+                'plugs_name' => $plugs_name
+            ])->find();
+    }
+
+
 }
