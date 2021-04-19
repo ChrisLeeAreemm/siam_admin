21d4f27929768826ab9094b36438e52f38203887
Chris <386273094@qq.com>
2021-04-19 15:38:24 +0800

    ADD :场景请求监控

---
+++ app/plugs/httpMonitor/service/SceneRequestMonitor.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * 场景请求监控
+ * User: Administrator
+ * Date: 2021/4/16
+ * Time: 20:07
+ */
+
+namespace app\plugs\httpMonitor\service;
+
+
+use app\facade\SiamApp;
+use app\facade\TimeHelper;
+use app\plugs\httpMonitor\model\PlugsHttpMonitorModel;
+use think\Model;
+use think\Request;
+
+class SceneRequestMonitor
+{
+    public static function run($request_sn,Request $request)
+    {
+        // 不监听plugs的请求
+        if (SiamApp::getInstance()->getModule() === 'plugs') return null;
+
+        $model = new PlugsHttpMonitorModel;
+        $model->path = $request->pathinfo();
+        $model->request_sn = $request_sn;
+        $model->request_content = serialize($request);
+        $model->response_content = null;
+        $model->create_time      = TimeHelper::get_now_ms();
+        $model->save();
+        return $model->id;
+    }
+}
