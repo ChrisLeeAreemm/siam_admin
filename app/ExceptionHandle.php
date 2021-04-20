231fdfc8083a7c7b8b79c2d14ce7c0cc0765f00f
Chris <386273094@qq.com>
2021-04-20 10:03:09 +0800

    ADD : 异常还原 , 异常记录组件

--- app/ExceptionHandle.php
+++ app/ExceptionHandle.php
@@ -1,9 +1,11 @@
 <?php
+
 namespace app;
 
 use app\exception\BaseException;
 use app\model\PlugsStatusModel;
 use app\plugs\base\service\PlugsBaseHelper;
+use app\plugs\exceptionLogger\model\PlugsExceptionLoggerModel;
 use think\db\exception\DataNotFoundException;
 use think\db\exception\ModelNotFoundException;
 use think\exception\Handle;
@@ -34,7 +36,7 @@ class ExceptionHandle extends Handle
      * 记录异常信息（包括日志或者其它方式记录）
      *
      * @access public
-     * @param  Throwable $exception
+     * @param Throwable $exception
      * @return void
      */
     public function report(Throwable $exception): void
@@ -47,30 +49,59 @@ class ExceptionHandle extends Handle
      * Render an exception into an HTTP response.
      *
      * @access public
-     * @param \think\Request   $request
-     * @param Throwable $e
+     * @param \think\Request $request
+     * @param Throwable      $e
      * @return Response
      */
     public function render($request, Throwable $e): Response
     {
         // 添加自定义异常处理机制
-        // TODO 是否安装异常记录插件，是则插入
-        if (PlugsBaseHelper::isInstall("exceptionLogger")){
+        // 是否安装异常记录插件，是则插入
+        if (PlugsBaseHelper::isInstall("exceptionLogger")) {
             $plugs_status = PlugsBaseHelper::getPlugsStatus("exceptionLogger");
-            if ($plugs_status['plugs_status'] == PlugsStatusModel::PLUGS_STATUS_ON){
-
+            if ($plugs_status['plugs_status'] == PlugsStatusModel::PLUGS_STATUS_ON) {
+                //序列化保存
+                $ExceptionLogger                  = new PlugsExceptionLoggerModel();
+                $ExceptionLogger->exception_class = get_class($e);
+                $ExceptionLogger->exception_date  = time();
+                $traces[] = [
+                    'name'    => get_class($e),
+                    'file'    => $e->getFile(),
+                    'line'    => $e->getLine(),
+                    'code'    => $this->getCode($e),
+                    'message' => $this->getMessage($e),
+                    'trace'   => $e->getTrace(),
+                    'source'  => $this->getSourceCode($e),
+                ];
+                $data = [
+                    'code'    => $this->getCode($e),
+                    'message' => $this->getMessage($e),
+                    'traces'  => $traces,
+                    'datas'   => $this->getExtendData($e),
+                    'tables'  => [
+                        'GET Data'            => $this->app->request->get(),
+                        'POST Data'           => $this->app->request->post(),
+                        'Files'               => $this->app->request->file(),
+                        'Cookies'             => $this->app->request->cookie(),
+                        'Session'             => $this->app->exists('session') ? $this->app->session->all() : [],
+                        'Server/Request Data' => $this->app->request->server(),
+                    ],
+                ];
+                $ExceptionLogger->exception_data  = json_encode($data);
+                $ExceptionLogger->create_time     = date('Y-m-d H:i:s');
+                $ExceptionLogger->save();
             }
         }
 
-        
-        if ($e instanceof BaseException){
+
+        if ($e instanceof BaseException) {
             return json([
                 'code' => $e->getCode(),
                 'data' => $e->get_return(),
                 'msg'  => $e->getMessage(),
             ]);
         }
-        if ($e instanceof ValidateException){
+        if ($e instanceof ValidateException) {
             return json([
                 'code' => $e->getCode(),
                 'data' => [],
---
+++ app/plugs/exceptionLogger/model/PlugsExceptionLoggerModel.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by CurdPlugs
+ * User: Chris
+ */
+
+namespace app\plugs\exceptionLogger\model;
+
+use app\model\BaseModel;
+use think\Model;
+
+#start
+/**
+ * bar_plugs_exception_logger
+ * @property mixed id                 自增ID
+ * @property mixed exception_class    异常类名(异常类型)
+ * @property mixed exception_date     异常日期
+ * @property mixed exception_data     异常内容
+ * @property mixed create_time        创建时间
+ */
+#end
+class PlugsExceptionLoggerModel extends BaseModel
+{
+    protected $name = 'plugs_exception_logger';
+    protected $pk   = 'id';
+    protected $type = [
+        'exception_date'  =>  'timestamp',
+    ];
+}
