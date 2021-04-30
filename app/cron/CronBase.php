59d8d9d6f39a3908cab155710f06c227a2c3e153
Chris <386273094@qq.com>
2021-04-30 10:14:56 +0800

    ADD : cron - 在线开关

--- app/cron/CronBase.php
+++ app/cron/CronBase.php
@@ -16,41 +16,6 @@ abstract class CronBase extends \Siam\AbstractInterface\CronBase
     abstract function run_period(): CronExpression;
 
     /**
-     * 在线开关功能 传入类名数组
-     * @param array $className
-     * @return bool
-     */
-    public function changeRun(array $className): bool
-    {
-        $file      = runtime_path() . 'cron_status.php';
-        $className = json_encode($className);
-        if (!file_exists($file)) {
-
-            $content = <<<EOL
-<?php
-namespace app\\runtime;
-return
-##start
-
-##end
-;
-EOL;
-            $write   = file_put_contents($file, $content);
-            if (!$write) {
-                return false;
-            }
-        }
-        $content = file_get_contents($file);
-        //更新文件
-        $content = preg_replace('/##start.*##end/sm', $className, $content);
-        $write = file_put_contents($file, $content);
-        if (!$write) {
-            return false;
-        }
-        return true;
-    }
-
-    /**
      * 执行逻辑
      * @throws \Throwable
      */
