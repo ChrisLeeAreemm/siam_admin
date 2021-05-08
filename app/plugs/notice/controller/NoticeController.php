891813a7c534d411d95e4a9a610219924dc6307a
Chris <386273094@qq.com>
2021-05-08 16:22:11 +0800

    Optimize : unread_count update

--- app/plugs/notice/controller/NoticeController.php
+++ app/plugs/notice/controller/NoticeController.php
@@ -119,7 +119,14 @@ class NoticeController extends PlugsBaseController
                 return $this->send(ErrorCode::DB_EXCEPTION, [], 'SAVE_READ_FAIL');
             }
         }
-        return $this->send(ErrorCode::SUCCESS, $notice, 'SUCCESS');
+        //更新阅读数据
+        $where_receiver['notice_receiver'] = PlugsNoticeModel::NOTICE_RECEIVER_ALL;
+        $whereOr[]                         = ['notice_receiver', 'like', "%\"{$this->who->u_id}\"%"];
+        $count                             = PlugsNoticeModel::where($where_receiver)->whereOr($whereOr)->count();
+        $is_read                           = PlugsNoticeReadModel::where('u_id', '=', $this->who->u_id)->count();
+        $unread_count                      = $count - $is_read;
+
+        return $this->send(ErrorCode::SUCCESS, ['notice'=>$notice,'unread_count'=>$unread_count], 'SUCCESS');
 
     }
 
