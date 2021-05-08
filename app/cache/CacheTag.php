792a974fe26b82bdb08eb1c8ea17161ac7cc3a6f
宣言 <59419979@qq.com>
2021-05-08 08:49:44 +0800

    add: cache tag

--- app/cache/CacheTag.php
+++ app/cache/CacheTag.php
@@ -7,4 +7,6 @@ class CacheTag
 {
     // 用户信息  =  USER_INFO_[用户id]
     const USER_INFO = "USER_INFO";
+    // 插件命令行运行状态 = PLUGS_COMMAND_RUN_STATUS_[插件名]，值为发送的心跳包时间戳
+    const PLUGS_COMMAND_RUN_STATUS = "PLUGS_COMMAND_RUN_STATUS";
 }
