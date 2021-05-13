82f4b947d0b3ed2f02aba27153f7b4fbbe8abc6f
Chris <386273094@qq.com>
2021-05-13 17:42:23 +0800

    ADD : cmsPlugs

---
+++ app/plugs/cms/Plugs.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\plugs\cms;
+
+use app\plugs\PlugsBase;
+use app\plugs\PlugsConfig;
+use think\facade\Route;
+
+class Plugs extends PlugsBase
+{
+
+
+    public function get_config(): PlugsConfig
+    {
+        $config = new PlugsConfig();
+        $config->setName("cms");
+        $config->setHandleModule(["admin", "plugs"]);// 只有admin模块才会执行初始化
+        $config->setMenu([
+            [
+                'title'  => "cms",
+                'href'   => "/plugs/cms/index",
+                'icon'   => "fa fa-tachometer",
+                'target' => '_self',
+            ],
+        ]);
+        return $config;
+    }
+
+    public function install()
+    {
+
+    }
+
+    public function remove()
+    {
+
+    }
+
+    public function init()
+    {
+        // 在这里注入路由[api] 等事件
+        Route::get('plugs/cms/index', function () {
+            return $this->pre_render_file(__DIR__ . "/view/index.html");
+        });
+        Route::any('plugs/cms/api/get_list', 'app\plugs\cms\controller\CmsController@get_list');
+    }
+}
