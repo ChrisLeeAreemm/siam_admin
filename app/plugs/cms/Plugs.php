4bf9fc86231d508e09b6fccd6cf248378c11f0b3
Chris <386273094@qq.com>
2021-05-16 21:03:45 +0800

    add ：DDL ;

--- app/plugs/cms/Plugs.php
+++ app/plugs/cms/Plugs.php
@@ -32,19 +32,43 @@ class Plugs extends PlugsBase
 
     public function install()
     {
-        // TODO 文章
+        // 文章
         PlugsDatabaseHelper::run(PlugsDatabaseHelper::create_ddl("plugs_cms_article", function (Table $table){
-
+            $table->setIfNotExists()->setTableComment('文章列表');          //设置表名称
+            $table->setTableCharset(Character::UTF8MB4_GENERAL_CI);      //设置表字符集
+            $table->setTableEngine(Engine::INNODB);                      //设置表引擎
+            $table->int('article_id')->setIsUnsigned()->setIsAutoIncrement()->setIsPrimaryKey()->setColumnComment('ID');
+            $table->varchar('article_title', 255)->setColumnComment("文章标题");
+            $table->text('article_content')->setColumnComment("文章内容");
+            $table->int('article_category_id')->setDefaultValue(0)->setColumnComment("文章分类ID");
+            $table->int('article_script_id')->setDefaultValue(0)->setColumnComment("文章脚本ID");
+            $table->int('article_author_id')->setColumnComment("作者ID");
+            $table->tinyint('article_status',3)->setDefaultValue(0)->setColumnComment("文章状态 0:草稿 1：发布");
+            $table->datetime("update_time")->setColumnComment("更新时间");
+            $table->datetime("create_time")->setColumnComment("创建时间");
         }));
 
-        // TODO 文章分类
+        // 文章分类
         PlugsDatabaseHelper::run(PlugsDatabaseHelper::create_ddl("plugs_cms_article_category", function (Table $table){
-
+            $table->setIfNotExists()->setTableComment('文章分类');          //设置表名称
+            $table->setTableCharset(Character::UTF8MB4_GENERAL_CI);      //设置表字符集
+            $table->setTableEngine(Engine::INNODB);                      //设置表引擎
+            $table->int('article_category_id')->setIsUnsigned()->setIsAutoIncrement()->setIsPrimaryKey()->setColumnComment('ID');
+            $table->varchar('article_category_name', 255)->setColumnComment("文章分类名");
+            $table->datetime("update_time")->setColumnComment("更新时间");
+            $table->datetime("create_time")->setColumnComment("创建时间");
         }));
 
-        // TODO 文章脚本
+        // 文章脚本
         PlugsDatabaseHelper::run(PlugsDatabaseHelper::create_ddl("plugs_cms_article_script", function (Table $table){
-
+            $table->setIfNotExists()->setTableComment('文章脚本');          //设置表名称
+            $table->setTableCharset(Character::UTF8MB4_GENERAL_CI);      //设置表字符集
+            $table->setTableEngine(Engine::INNODB);                      //设置表引擎
+            $table->int('article_script_id')->setIsUnsigned()->setIsAutoIncrement()->setIsPrimaryKey()->setColumnComment('ID');
+            $table->varchar('article_script_name', 255)->setColumnComment("脚本名");
+            $table->varchar('article_script_content', 255)->setColumnComment("脚本内容");
+            $table->datetime("update_time")->setColumnComment("更新时间");
+            $table->datetime("create_time")->setColumnComment("创建时间");
         }));
     }
 
