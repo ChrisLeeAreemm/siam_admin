93aec17aff5dbb0a66cf0ce8c6a2adaba2fbcbb1
Chris <386273094@qq.com>
2021-05-17 11:25:59 +0800

    OPTIMIZE : model

--- app/model/AuthsModel.php
+++ app/model/AuthsModel.php
@@ -12,6 +12,7 @@ namespace app\model;
  * @property mixed auth_id	权限id
  * @property mixed auth_name	权限名
  * @property mixed auth_rules	路由地址
+ * @property mixed auth_icon	路由图标
  * @property mixed auth_type	权限类型 0菜单1按钮
  * @property mixed create_time	创建时间
  * @property mixed update_time	更新时间
