aff9ece0364602a606cfc464a01f9dba8a28a916
何泽宏 <59419979@qq.com>
2022-06-11 15:26:57 +0800

    引入phpsheet依赖; 修复API无效问题

--- public/admin/js/lay-module/setter.js
+++ public/admin/js/lay-module/setter.js
@@ -4,8 +4,8 @@
 
 layui.define(['layer'], function (exports) {
     exports('setter', {
-        // Api : "__API__"
-        Api : "http://127.0.0.1:8000/index.php/"
+        Api : "__API__"
+        // Api : "http://127.0.0.1:8000/index.php/"
         ,name: '__项目名__'
         , tableName: 'Siam_Admin' //本地存储表名
         //自定义请求字段
