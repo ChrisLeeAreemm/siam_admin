1e3b9747dad565f7396d557a9fb9dc7b1d0d6a7d
Chris <386273094@qq.com>
2023-01-08 18:22:07 +0800

    optimize : Default API address

--- public/admin/js/lay-module/setter.js
+++ public/admin/js/lay-module/setter.js
@@ -4,8 +4,7 @@
 
 layui.define(['layer'], function (exports) {
     exports('setter', {
-        Api : "http://127.0.0.1:8999/index.php/"
-        // Api : "http://127.0.0.1:8000/index.php/"
+        Api : "http://127.0.0.1:8000/index.php/"
         ,name: 'SiamAdmin'
         , tableName: 'Siam_Admin' //本地存储表名
         //自定义请求字段
